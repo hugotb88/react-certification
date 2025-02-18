57de487c4e7641d9b07033d2690c8abbc5179be8
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-02-18 13:51:18 -0600

    memo keyword concepts

---
+++ src/Components/Memo.js
@@ -0,0 +1,111 @@
+/*
+    Memo
+
+    Note ** This also uses Hooks.
+    - Using 'memo' will cause React to skip rendering a component if its props have not changed.
+    - This can improve performance.
+*/
+
+// Problem //
+// 'Todos' component re-renders even when the to-dos have not changed.
+
+//  index.js
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+//  Todos.js
+const Todos = ({ todos }) => {
+    console.log("child render");
+    return (
+      <>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+      </>
+    );
+  };
+  
+  export default Todos;
+
+
+
+// -------------------------- Solution ------------------- //
+/* 
+    - To fix this, we can use 'memo'.
+    - Use 'memo' to keep the Todos component from needlessly re-rendering.
+    - Wrap the Todos component export in 'memo'
+    - Now the Todos component only re-renders when the todos that are passed to it through props are updated.
+*/
+
+
+// index.js
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+// Todos.js
+import { memo } from "react";
+
+const Todos = ({ todos }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+    </>
+  );
+};
+
+export default memo(Todos);
