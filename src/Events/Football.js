0944d7a1a55101d52ec6d528de546f653a677968
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-02-20 13:50:08 -0600

    Event Object

--- src/Events/Football.js
+++ src/Events/Football.js
@@ -1,14 +1,20 @@
 /*
     Example of Event in React
     Passing arguments to an event handler using an arrow function
+
+    The Event object
 */
 function Football(){
-    const shoot = (a) => {
-        alert(a);
+    const shoot = (a, b) => {
+        alert(b.type);
+        /*
+            'b' represents the React event that triggered the funciton.
+            In this case the 'click' event
+        */
     }
 
     return (
-        <button onClick={() => shoot("Gol perros, goooool")}> Take the shoot </button>
+        <button onClick={(event) => shoot("Gol perros, goooool", event)}> Take the shoot </button>
     );
 }
 
