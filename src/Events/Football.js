38e9f348d4717417bcf472f14798b1b705864c30
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-02-20 13:47:39 -0600

    Passing Arguments to an Event Handler

--- src/Events/Football.js
+++ src/Events/Football.js
@@ -1,13 +1,14 @@
 /*
     Example of Event in React
+    Passing arguments to an event handler using an arrow function
 */
 function Football(){
-    const shoot = () => {
-        alert("Al angulo no vale papaw");
+    const shoot = (a) => {
+        alert(a);
     }
 
     return (
-        <button onClick={shoot}> Take the shoot </button>
+        <button onClick={() => shoot("Gol perros, goooool")}> Take the shoot </button>
     );
 }
 
